2bea585b6bdcb6bf4ca91675119b7123c660b93f
Jaydip Kumbhani <kumbhanijaydeep007@gmail.com>
2022-11-12 23:49:42 +0530

    Implemented user list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Detail, UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'service';
+export class AppComponent implements OnInit {
+
+  title: string;
+  msg: any = [];
+
+  constructor(private userservice: UserService) { }
+
+  ngOnInit() {
+    this.setTitle();
+    this.getUserList();
+  }
+
+  setTitle() {
+    this.title = this.userservice.title;
+  }
+
+  getUserList() {
+    this.userservice.getUserList().subscribe((data) => {
+      this.msg = data;
+      console.log(data)
+    });
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
