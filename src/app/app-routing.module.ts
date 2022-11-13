1430e14ba6a99fa0afa27be87a8c152b48e246e7
Tusharbalar <balar.tushar1@gmail.com>
2022-11-13 11:17:11 +0530

    Updated app routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './users/list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user-list', component: UserListComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/user-list'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
